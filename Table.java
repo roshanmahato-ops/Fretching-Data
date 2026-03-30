64afe2e3e09094717aae292f82533b378766b212
roshanmahato-ops <roshabmahato@gmail.com>
2026-03-30 07:04:56 +0530

    Add message

--- Table.java
+++ Table.java
@@ -1,7 +1,6 @@
 public class Table {
+
     public static void main(String[] args) {
-        for (int i = 1; i <=10; i++) {
-            System.out.println(i*5);
-        }
+        System.out.println("Hello Table");
     }
 }
